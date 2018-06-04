b0a21648c2f1e916d55d748cd0707a21e17834bc
milan.zivanov <milan.zivanov@gmail.com>
2018-06-04 16:37:00 +0200

    fxLayout and signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -28,7 +30,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule,
-         MatIconModule } from '@angular/material';
+         MatIconModule,
+         MatFormFieldModule,
+         MatInputModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule,
+            MatIconModule,
+            MatInputModule,
+            MatFormFieldModule],
+  exports: [MatButtonModule,
+            MatIconModule,
+            MatInputModule,
+            MatFormFieldModule],
 })
 export class MaterialModule {}
